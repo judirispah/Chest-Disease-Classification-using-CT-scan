3674f7d818e5183144be2bb3eed5bef89c480282
judirispah <judi.rispah.0606@gmail.com>
2024-03-27 03:29:47 +0530

    prediction pipeline updated

--- src/cnnClassifier/pipeline/prediction.py
+++ src/cnnClassifier/pipeline/prediction.py
@@ -2,25 +2,30 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import os
+from keras.applications.vgg16 import preprocess_input
+#from cnnClassifier.entity.config_entity import TrainingConfig
 
 
 
 class PredictionPipeline:
     def __init__(self,filename):
         self.filename =filename
-
+        
+ 
 
     
     def predict(self):
         ## load model
         
         # model = load_model(os.path.join("artifacts","training", "model.h5"))
-        model = load_model(os.path.join("model", "model.h5"))
+        model = load_model(r"C:\Users\judirispah\Chest-Disease-Classification-using-CT-scan\artifacts\training\model.h5")
 
         imagename = self.filename
         test_image = image.load_img(imagename, target_size = (224,224))
         test_image = image.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis = 0)
+        test_image = preprocess_input(test_image)
+        test_image=test_image/225
         result = np.argmax(model.predict(test_image), axis=1)
         print(result)
 
